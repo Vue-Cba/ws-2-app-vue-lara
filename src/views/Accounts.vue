b9b46103f19b8e556fd8bc047aca76edcfa3862d
selenel21 <56981657+selenel21@users.noreply.github.com>
2020-02-03 10:52:00 -0300

    Add files via upload

--- src/views/Accounts.vue
+++ src/views/Accounts.vue
@@ -1,13 +1,64 @@
 <template>
-    <h1>Accounts</h1>
+    <div>
+        <v-app-bar
+                color="blue-grey darken-2"
+                dense
+                dark>
+
+            <v-toolbar-title>Accounts</v-toolbar-title>
+
+            <v-spacer/>
+            <v-btn>{{userEmail}}</v-btn>
+            <v-btn icon @click="logout">
+                <v-icon>mdi-logout</v-icon>
+            </v-btn>
+        </v-app-bar>
+
+        <v-container class="accounts">
+            <v-row>
+                <v-col>
+                    <accounts-form/>
+                </v-col>
+
+                <v-col>
+                    <account-list/>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
 </template>
 
 <script>
+    import AccountsForm from "../components/AccountsForm";
+    import AccountList from "../components/AccountList";
+
     export default {
-        name: "Accounts"
+        name: "Accounts",
+        components: {AccountList, AccountsForm},
+
+        computed: {
+            userEmail() {
+                return localStorage.getItem('user-username')
+
+            }
+        },
+
+        methods: {
+            logout() {
+                localStorage.removeItem('user-token');
+                this.$router.go('/')
+            }
+        }
     }
+
+
 </script>
 
-<style scoped>
+<style lang="scss">
 
+    .accounts {
+        padding-top: 200px;
+        padding-bottom: 63px;
+
+    }
 </style>
